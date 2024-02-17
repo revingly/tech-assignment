b6f191195693a3016db498f28e3b57b1236b5a23
revingly <revaxl09@gmail.com>
2024-02-17 20:44:46 +0900

    Add routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\SendEmailVerificationNotificationController;
+use App\Http\Controllers\Auth\SendPasswordResetLinkController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +19,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('movie', [MovieController::class, 'index'])->name('movie.index');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('me', [AuthController::class, 'me'])->name('me');
+
+});
+
+Route::post('/forgot-password', SendPasswordResetLinkController::class)
+    ->name('password.email');
+
+Route::post('/reset-password', ResetPasswordController::class)
+    ->name('password.update');
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/email/verification-notification', SendEmailVerificationNotificationController::class)
+        ->middleware(['throttle:6,1'])
+        ->name('verification.send');
+
+    Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
 });
