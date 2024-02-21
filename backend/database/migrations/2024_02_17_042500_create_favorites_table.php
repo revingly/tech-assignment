80ac97d0042c229478096c2dcbae1e09ebebc462
revingly <revaxl09@gmail.com>
2024-02-21 15:04:34 +0900

    Remove timestamps from the pivot table

--- backend/database/migrations/2024_02_17_042500_create_favorites_table.php
+++ backend/database/migrations/2024_02_17_042500_create_favorites_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('movie_id');
-            $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
