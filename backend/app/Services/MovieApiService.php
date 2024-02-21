06905413809295dfe6b915e252c0f8c35ba03259
revingly <revaxl09@gmail.com>
2024-02-21 15:05:25 +0900

    Add popular and trending movies function for the API service

--- backend/app/Services/MovieApiService.php
+++ backend/app/Services/MovieApiService.php
@@ -2,33 +2,53 @@
 
 namespace App\Services;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
-use Psr\Http\Message\ResponseInterface;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Http;
 
 class MovieApiService
 {
 
-    private Client $httpClient;
     private string $apiKey;
+    private string $baseUrl;
 
     public function __construct()
     {
-        $this->httpClient = new Client([
-            'base_uri' => config('api.endpoints.movie.url'),
-            'headers' => ['Accept'=> 'application/json']
-        ]);
+        $this->baseUrl = config('api.endpoints.movie.url');
         $this->apiKey = config('api.endpoints.movie.api_key');
     }
 
     /**
      * @param string $name
-     * @return ResponseInterface
-     * @throws GuzzleException
+     * @return array
+     * @throws RequestException
      */
-    public function findBy(string $name): ResponseInterface
+    public function findBy(string $name): array
     {
 
-        return $this->httpClient->get(sprintf("search/movie?api_key=%s&query=%s", $this->apiKey, $name), []);
+        $response = Http::get(sprintf("%ssearch/movie?api_key=%s&query=%s", $this->baseUrl, $this->apiKey, $name));
+        $response->throwUnlessStatus(200);
+        return $response->json();
+    }
+
+    /**
+     * @return array
+     * @throws RequestException
+     */
+    public function popular(): array
+    {
+        $response = Http::movie()->get(sprintf("movie/popular?api_key=%s", $this->apiKey));
+        $response->throwUnlessStatus(200);
+        return $response->json();
+    }
+
+    /**
+     * @return array
+     * @throws RequestException
+     */
+    public function trending(): array
+    {
+        $response = Http::movie()->get(sprintf("trending/movie/week?api_key=%s", $this->apiKey));
+        $response->throwUnlessStatus(200);
+        return $response->json();
     }
 }
