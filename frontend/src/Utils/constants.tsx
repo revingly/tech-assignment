7dce0f4793f203148c4318c65d815c830b34e8a9
revingly <revaxl09@gmail.com>
2024-02-23 13:08:35 +0900

    Add constant for backend api url

--- frontend/src/Utils/constants.tsx
+++ frontend/src/Utils/constants.tsx
@@ -1,2 +1,3 @@
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 export const MOVIES_PAGE_TITLE = 'Current popular movies';
+export const BACKEND_API_URL = 'http://localhost:8000/api'; // running on port 8000 if not running inside docker container
