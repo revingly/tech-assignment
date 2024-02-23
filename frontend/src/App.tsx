f9a00287bd54c0e3f6f04ab2da3a5c8ad5f5ac6e
revingly <revaxl09@gmail.com>
2024-02-23 13:00:44 +0900

    Update App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { Movies } from './pages/Movies';
+import Home from './pages/Home';
+import AuthProvider from "./context/AuthContext";
+import Protected from './components/Protected';
+import { MovieDetails } from './pages/MovieDetails';
+import { Favorites } from './pages/Favorites';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ChakraProvider>
 
-export default App
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/movies" element={<Protected><Movies /></Protected>} />
+            <Route path="/movies/:movieId" element={<Protected><MovieDetails /></Protected>} />
+            <Route path="/favorites" element={<Protected><Favorites /></Protected>} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </ChakraProvider>
+  );
+};
+
+export default App;
